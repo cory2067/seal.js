939835304b5f871ce5384611d8bf49b0467e4b46
Cory Lynch <cjl2625@gmail.com>
2018-11-25 21:48:17 -0500

    Add object template.

---
+++ addon.js
@@ -0,0 +1,16 @@
+var addon = require('node-cmake')('sealjs');
+
+var obj = new addon.MyObject(10);
+console.log( obj.plusOne() ); // 11
+console.log( obj.plusOne() ); // 12
+console.log( obj.plusOne() ); // 13
+
+console.log( obj.multiply().value() ); // 13
+console.log( obj.multiply(10).value() ); // 130
+
+var newobj = obj.multiply(-1);
+console.log( newobj.value() ); // -13
+console.log( obj === newobj ); // false
+
+var a = {meme: "hi there im pasing stuf"};
+console.log(obj.lookAt(a))
